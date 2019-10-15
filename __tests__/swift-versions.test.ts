d297b030daf567195556c5c469e7070e4d639b3b
Frederik Wallner <frederik.wallner@gmail.com>
2019-10-15 21:03:57 +0200

    Add tests for version resolver

---
+++ __tests__/swift-versions.test.ts
@@ -0,0 +1,47 @@
+import * as versions from '../src/swift-versions'
+
+describe('swift version resolver', () => {
+  it('identifies X.X.X versions', async () => {
+    const version = await versions.verify('5.0.3')
+    expect(version).toBe('5.0.3')
+  })
+
+  it('identifies X.X.0 versions', async () => {
+    const version = await versions.verify('5.0.0')
+    expect(version).toBe('5.0')
+  })
+
+  it('identifies X.X versions', async () => {
+    const version = await versions.verify('5.0')
+    expect(version).toBe('5.0.3')
+  })
+
+  it('identifies ~X.X versions', async () => {
+    const version = await versions.verify('~5.0')
+    expect(version).toBe('5.0.3')
+  })
+
+  it('identifies X versions', async () => {
+    const version = await versions.verify('5')
+    expect(version).toBe('5.1.1')
+  })
+
+  it('throws an error if version is invalid', async () => {
+    expect.assertions(1);
+    try {
+      await versions.verify('foo')
+    } catch (e) {
+      expect(e).toEqual(new Error('Version must be a valid semver format.'))
+    }
+  })
+
+  it('throws an error if no matching version is found', async () => {
+    expect.assertions(1);
+    try {
+      await versions.verify('1.0')
+    } catch (e) {
+      expect(e).toEqual(new Error('Version "1.0" is not available'))
+    }
+  })
+
+})

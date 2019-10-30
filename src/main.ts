0b059a4a93662e0f3d2630932a2bf98bb572dbb3
Frederik Wallner <frederik.wallner@gmail.com>
2019-10-30 09:44:53 +0100

    Debug log os

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,21 @@ import * as core from '@actions/core'
 import * as system from './os'
 import * as versions from './swift-versions'
 import * as linux from './linux-install'
+import * as ops from 'os'
+import getos from 'getos'
 
 async function run() {
   let version = versions.verify(core.getInput('swift-version', { required: true }))
   let os = core.getInput('os', { required: true })
 
   console.log(`This platform is ${process.platform}`)
-  console.log(process.env['RUNNER_OS']) 
+  console.log(process.env['RUNNER_OS'])
+
+  console.log(ops.release())
+
+  getos((_, os) => {
+    console.log(JSON.stringify(os))
+  })
 
   let platform = system.getSystem(os)
   switch (platform.os) {

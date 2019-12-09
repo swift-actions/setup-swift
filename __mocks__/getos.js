5c8a2577eaa70ca6cd2e8e596edf4f2a4c4af423
Frederik Wallner <frederik.wallner@gmail.com>
2019-12-09 21:11:06 +0100

    Update os tests

---
+++ __mocks__/getos.js
@@ -0,0 +1,18 @@
+const getos = jest.genMockFromModule('getos')
+
+let system = null
+
+function __setSystem(newSystem) {
+  system = newSystem
+}
+
+const mockGetos = function (callback) {
+  callback(null, system)
+}
+
+mockGetos.__setSystem = function (newSystem) {
+  system = newSystem
+}
+
+
+module.exports = mockGetos
--- __tests__/os.test.ts
+++ __tests__/os.test.ts
@@ -1,47 +1,43 @@
 import * as os from '../src/os'
 
+jest.mock('getos')
+
+const setSystem = require('getos').__setSystem
+
 describe('os resolver', () => {
-  it('finds matching system and version', () => {
-    let ubuntu = os.getSystem('ubuntu-18.04')
+  it('finds matching system and version', async () => {
+    setSystem({ os: 'linux', dist: 'Ubuntu Linux', release: '18.04' })
+
+    let ubuntu = await os.getSystem()
     expect(ubuntu.os).toBe(os.OS.Ubuntu)
     expect(ubuntu.version).toBe('18.04')
-    expect(ubuntu.name).toBe('ubuntu-18.04')
+    expect(ubuntu.name).toBe('Ubuntu')
 
-    let mac = os.getSystem('macOS-latest')
+    setSystem({ os: 'darwin', dist: 'macOS', release: 'latest' })
+
+    let mac = await os.getSystem()
     expect(mac.os).toBe(os.OS.MacOS)
     expect(mac.version).toBe('latest')
-    expect(mac.name).toBe('macOS-latest')
+    expect(mac.name).toBe('macOS')
   })
 
-  it('throws an error if the provided system is invalid', () => {
-    expect.assertions(2)
-    try {
-      os.getSystem('')
-    } catch (e) {
-      expect(e).toEqual(new Error('Provided os "" not valid'))
-    }
-    try {
-      os.getSystem('foobar')
-    } catch (e) {
-      expect(e).toEqual(new Error('Provided os "foobar" not valid'))
-    }
-  })
-
-  it('throws an error if the os is not supported', () => {
+  it('throws an error if the os is not supported', async () => {
+    setSystem({ os: 'windows', dist: 'Microsoft Windows 10', release: '10.0' })
     expect.assertions(1)
     try {
-      os.getSystem('foobar-1.0')
+      await os.getSystem()
     } catch (e) {
-      expect(e).toEqual(new Error('"foobar" is not a supported platform'))
+      expect(e).toEqual(new Error('"windows" is not a supported platform'))
     }
   })
 
-  it('throws an error if the version is not supported', () => {
+  it('throws an error if the version is not supported', async () => {
+    setSystem({ os: 'linux', dist: 'Ubuntu Linux', release: '15.04' })
     expect.assertions(1)
     try {
-      os.getSystem('macOS-0.1')
+      await os.getSystem()
     } catch (e) {
-      expect(e).toEqual(new Error('Version "0.1" of macOS is not supported'))
+      expect(e).toEqual(new Error('Version "15.04" of Ubuntu is not supported'))
     }
   })
 })

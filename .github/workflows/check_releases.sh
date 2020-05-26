b5d6f37608fe0c64569e2a8a3d6b794c07c9dd7c
Frederik Wallner <frederik.wallner@gmail.com>
2020-05-26 22:04:46 +0200

    Add workflow that checks for new swift releases

---
+++ .github/workflows/check_releases.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+latest_release_name=`hub api repos/apple/swift/releases/latest | jq '.name'`
+exp="[0-9\.]"
+re="Swift ([0-9\.]+)"
+if [[ $latest_release_name =~ $re ]]
+then 
+  latest_release_number=${BASH_REMATCH[1]}
+else
+  echo "No release found"
+  exit 0
+fi
+
+echo "Latest release is $latest_release_number"
+
+if grep -q $latest_release_number "./src/swift-versions.ts"
+then
+  echo "Version already added"
+  exit 0
+fi
+
+if hub issue | grep "Add Swift $latest_release_number"
+then
+  echo "Issue already created"
+  exit 0
+fi
+
+hub issue create -m "Add Swift $latest_release_number" -m "Swift $latest_release_number have been released and should be added" -a "fwal" -l "enhancement"
+echo "Issue created"

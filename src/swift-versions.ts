be09a5e898369ebea4fa755e2c8d89f219a5d5a1
Frederik Wallner <frederik.wallner@gmail.com>
2021-09-13 07:40:59 +0200

    Add Swift 5.4.x versions

--- src/swift-versions.ts
+++ src/swift-versions.ts
@@ -3,6 +3,9 @@ import * as core from "@actions/core";
 import { System, OS } from "./os";
 
 const VERSIONS_LIST: [string, OS[]][] = [
+  ["5.4.3", [OS.MacOS, OS.Ubuntu]],
+  ["5.4.2", [OS.MacOS, OS.Ubuntu]],
+  ["5.4.1", [OS.MacOS, OS.Ubuntu]],
   ["5.4", [OS.MacOS, OS.Ubuntu]],
   ["5.3.3", [OS.MacOS, OS.Ubuntu]],
   ["5.3.2", [OS.MacOS, OS.Ubuntu]],

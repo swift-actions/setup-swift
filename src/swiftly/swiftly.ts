c503ed131b057a23f954b80369889d518c2daae9
Frederik Wallner <frederik.wallner@gmail.com>
2025-04-25 10:21:06 +0200

    Avoid modifying profile as it have no effect

--- src/swiftly/swiftly.ts
+++ src/swiftly/swiftly.ts
@@ -36,6 +36,7 @@ export async function installSwift(version: string) {
     "--skip-install",
     "--quiet-shell-followup",
     "--assume-yes",
+    "-–no-modify-profile",
   );
 
   info(`Installing Swift ${version}`);

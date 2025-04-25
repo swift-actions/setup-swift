5afa18a3a8384c6d4cd4ba54d0aabcf9c425180f
Frederik Wallner <frederik.wallner@gmail.com>
2025-04-25 08:00:26 +0200

    Check variables after init

--- src/swiftly/swiftly.ts
+++ src/swiftly/swiftly.ts
@@ -1,4 +1,4 @@
-import { info } from "@actions/core";
+import { debug, info } from "@actions/core";
 import { cmd } from "../core";
 
 async function swiftly(...args: string[]) {
@@ -17,6 +17,9 @@ export async function installSwift(version: string) {
     "--quiet-shell-followup",
     "--assume-yes",
   );
+
+  debug(`SWIFTLY_HOME_DIR: ${process.env.SWIFTLY_HOME_DIR}`);
+  debug(`SWIFTLY_BIN_DIR: ${process.env.SWIFTLY_BIN_DIR}`);
 
   info(`Installing Swift ${version}`);
   await swiftly("install", "--use", version, "--assume-yes");

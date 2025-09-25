bb9d928d17930db5f808fa68520e2dc2e0ff1f2c
Martin Man <mman@martinman.net>
2025-09-25 19:47:50 +0200

    fix: Handle compressed/noncompressed GPG keys in robust way

--- src/gpg.ts
+++ src/gpg.ts
@@ -8,8 +8,12 @@ export async function setupKeys() {
     "https://swift.org/keys/all-keys.asc"
   );
 
+  core.debug("Examining verification keys");
+  await exec(`file "${path}"`);
+  const isPlaintext = await exec(`gunzip --test "${path}"`, undefined, { silent: true, ignoreReturnCode: true });
+
   core.debug("Importing verification keys");
-  await exec(`gpg --import "${path}"`);
+  await exec('bash', ['-c', `${isPlaintext ? "cat" : "zcat"} "${path}" | gpg --import`]);
 
   core.debug("Refreshing keys");
   await refreshKeys();
